29954b3d786d3d57cee63c6d8003bff5b0aedc0c
Fernando Arámburu <fernandoaramburu@gmail.com>
2017-06-26 00:38:57 -0300

    Change .NET Standard 1.4 project. Add another test project for .NET Framework 4.6.1 as some tests where failing and I needed to understand what was going on. Everything fixed

---
+++ Nuba.Finance.Google.Test.Net461/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nuba.Finance.Google.Test.Net461")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nuba.Finance.Google.Test.Net461")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5dbf81a4-de49-4d10-8de4-164ca00e855b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
